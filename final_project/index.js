216d8a773453e9fd58bcee7c42960144b6926208
mathiaspellegrin <mathias.pellegrin.pro@gmail.com>
2024-08-10 19:50:35 +0200

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,33 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+  secret: "fingerprint_customer",
+  resave: true,
+  saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+app.use("/customer/auth/*", function auth(req, res, next) {
+    const token = req.headers['authorization']?.split(' ')[1]; 
+  
+    if (!token) {
+      return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+  
+    jwt.verify(token, 'your_jwt_secret_key', (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      }
+  
+      req.user = decoded;
+      next();
+    });
+  });
+  
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
